26cf7185aeead068b909bac8e5350f8a4d65fe78
lehaianhsone <124845597+lehaianhsone@users.noreply.github.com>
2023-03-03 07:53:10 +0700

    Add files via upload

--- BT03/04.cpp
+++ BT03/04.cpp
@@ -3,13 +3,15 @@
 int main()
 {
     int m, n; std::cin >> m >> n;
-    char a[m][n];
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++) std::cin >> a[i][j];
+    char a[m + 2][n + 2];
+    for(int i = 0; i < m + 2; i++){
+        for(int j = 0; j < n + 2; j++) a[i][j] = '.';
     }
-
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 1; i < m + 1; i++){
+        for(int j = 1; j < n + 1; j++) std::cin >> a[i][j];
+    }
+    for(int i = 1; i <= m; i++){
+        for(int j = 1; j <= n; j++){
             if(a[i][j] == '*') std::cout << '*' << ' ';
             else{
                 int c = 0;
